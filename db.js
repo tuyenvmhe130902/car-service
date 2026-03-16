963d2f30fe38e0fdb97fa1f621fa43fb47a68bc6
tuyen.vumanh <tuyen.vumanh@vti.com.vn>
2026-03-16 15:17:50 +0700

    [DB]fix connect db

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import postgres from 'postgres'
 
-const connectionString = process.env.SUPABASE_DB_URL
+const connectionString = process.env.DATABASE_URL
 const sql = postgres(connectionString)
 
 export default sql
